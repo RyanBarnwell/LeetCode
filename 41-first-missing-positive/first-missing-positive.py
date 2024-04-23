0e59417dd3daf09c0badfd807a6c302588a91a4d
RyanBarnwell <132297652+RyanBarnwell@users.noreply.github.com>
2024-04-23 16:24:18 -0400

    Time: 523 ms (5.10%) | Memory: 43.3 MB (5.51%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        nums = np.array(nums)
+        #get all positive
+        nums = nums[nums>0]
+        nums = list(set(nums))
+        nums.sort()
+        print(nums)
+        
+        if(len(nums) == 0):
+            return 1
+        
+        for x in range(1, len(nums)+1):
+            if(nums[x-1] != x):
+                return x
+        return x+1
+        
