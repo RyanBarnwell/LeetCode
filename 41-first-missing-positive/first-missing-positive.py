ec271655bdb7c005f68e62180e7e50a172dd69d1
RyanBarnwell <132297652+RyanBarnwell@users.noreply.github.com>
2024-04-23 17:06:26 -0400

    Time: 455 ms (5.10%) | Memory: 35.1 MB (5.51%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,16 +1,33 @@
 import numpy as np
 class Solution(object):
     def firstMissingPositive(self, nums):
-        nums = np.array(nums)
-        #get all positive
-        nums = nums[nums>0]
-        nums = list(set(nums))
-        nums.sort()
-        if(len(nums) == 0):
+        length = len(nums)
+        #swap positive values to end of array
+        #pseudo-sort
+        pos = 0
+        for i in range(0, length):
+            if nums[i] <= 0:
+                nums[i], nums[pos] = nums[pos], nums[i]
+                pos += 1
+        #pos is negative index
+        #cut array at negative index
+        nums = nums[pos:]
+        
+        if not nums:
             return 1
         
-        for x in range(1, len(nums)+1):
-            if(nums[x-1] != x):
-                return x
-        return x+1
+        for i in range(0, len(nums)):
+            # check if number is at expected index and if it has been accessed
+            #removes need to remove duplicates
+            if(abs(nums[i]) - 1 < len(nums) and nums[abs(nums[i]) - 1] > 0):
+                #sets index to negative to mark indexing
+                nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1]
+
+        #use len to get current length
+        for i in range(0, len(nums)):
+            #return first index
+            if nums[i] > 0:
+                return i + 1
+
+        return len(nums) + 1
         
