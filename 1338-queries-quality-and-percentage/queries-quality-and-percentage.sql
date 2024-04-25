7b7f2b8bce8486b0d44ad51117dd91b9b03cde49
RyanBarnwell <132297652+RyanBarnwell@users.noreply.github.com>
2024-04-25 18:51:50 -0400

    Time: 613 ms (82.97%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -3,5 +3,5 @@ SELECT query_name,
     ROUND((SUM(rating/position)/COUNT(query_name)), 2) AS quality,
     ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100, 2) AS poor_query_percentage
 FROM Queries
-WHERE query_name is not null
+WHERE query_name IS NOT NULL
 GROUP BY query_name
